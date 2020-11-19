e49309a1db72d110efff79296a0e30194d325d08
Banto <antoine.balieu@outlook.com>
2020-11-19 16:33:17 +0100

    🚑 Fixed data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,10 +1,8 @@
 -- Enregistrements pour la table "BUG"
-INSERT INTO BUG
-VALUES "Syntaxe Erreur", "Un erreur de syntaxe génère un messsage d'erreur de l'application", "24th Sep 2019", "LOW", "DONE"
+INSERT INTO BUG (id, title, `description`, creation_date, priority, progress) VALUES
+    (1, 'Syntaxe Erreur', 'Un erreur de syntaxe génère un messsage d''erreur de l''application', '24th Sep 2019', 'LOW', 'DONE');
 
 -- Enregistrements pour la table "DEVELOPER"
-INSERT INTO DEVELOPER
-VALUES "qcarry", "Stelario"
-
-INSERT INTO DEVELOPER
-VALUES "aBalieu", "Banto"
+INSERT INTO DEVELOPER (id, handle, display_name) VALUES
+    (1, 'qcarry', 'Stelario'),
+    (2, 'aBalieu', 'Banto');
