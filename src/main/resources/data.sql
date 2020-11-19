7a8a1bedd706c59f4ec3ef3ef8c779af3bf7526c
Quentin Carry <quentincarry@gmail.com>
2020-11-19 16:03:43 +0100

    :hammer: Added data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,10 @@
+-- Enregistrements pour la table "BUG"
+INSERT INTO BUG
+VALUES "Syntaxe Erreur", "Un erreur de syntaxe génère un messsage d'erreur de l'application", "24th Sep 2019", "LOW", "DONE"
+
+-- Enregistrements pour la table "DEVELOPER"
+INSERT INTO DEVELOPER
+VALUES "qcarry", "Stelario"
+
+INSERT INTO DEVELOPER
+VALUES "aBalieu", "Banto"
