c53e34c076d37ce35caa757d7084a3ed9d1d837f
Banto <antoine.balieu@outlook.com>
2020-11-19 18:26:51 +0100

    🚨 Fixed serialization warning

--- src/main/java/org/bugtracker/api/exceptions/ResourceNotFoundException.java
+++ src/main/java/org/bugtracker/api/exceptions/ResourceNotFoundException.java
@@ -1,6 +1,7 @@
 package org.bugtracker.api.exceptions;
 
 public class ResourceNotFoundException extends Exception {
+    private static final long serialVersionUID = 1L;
 
     public ResourceNotFoundException(String message) {
         super(message);
