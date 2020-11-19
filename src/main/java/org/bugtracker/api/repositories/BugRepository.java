c0e5f42afe052e36aa139540a8a98a32724f43ce
Banto <antoine.balieu@outlook.com>
2020-11-19 18:22:41 +0100

    🚑 Fixed Springfox version

--- src/main/java/org/bugtracker/api/repositories/BugRepository.java
+++ src/main/java/org/bugtracker/api/repositories/BugRepository.java
@@ -4,7 +4,9 @@ import java.util.Optional;
 
 import org.bugtracker.api.models.Bug;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BugRepository extends JpaRepository<Bug, Integer> {
     Optional<Bug> findById(Integer id);
 }
--- src/main/java/org/bugtracker/api/repositories/DeveloperRepository.java
+++ src/main/java/org/bugtracker/api/repositories/DeveloperRepository.java
@@ -4,7 +4,9 @@ import java.util.Optional;
 
 import org.bugtracker.api.models.Developer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface DeveloperRepository extends JpaRepository<Developer, Integer> {
     Optional<Developer> findById(Integer id);
 }
