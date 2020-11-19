ab7cf77fa7e6dba02bfe25e52a8940ed34f32f68
Banto <antoine.balieu@outlook.com>
2020-11-19 16:00:32 +0100

    🔧 Added Spring Security configuration

---
+++ src/main/java/org/bugtracker/api/config/SecurityConfig.java
@@ -0,0 +1,17 @@
+package org.bugtracker.api.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/h2-console/**").permitAll()
+        .and().csrf().ignoringAntMatchers("/h2-console/**")
+        .and().headers().frameOptions().sameOrigin();
+	}
+}
